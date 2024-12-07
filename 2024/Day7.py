b5d01a7c4bab4289d426d70bacd3e4f949228d80
MargaridaMC <margaridamcosta.96@gmail.com>
2024-12-07 17:56:48 +0000

    Remove duplicate code

--- 2024/Day7.py
+++ 2024/Day7.py
@@ -1,56 +1,45 @@
-from itertools import pairwise
-
 def parse_input(data):
     equations = [row.split(": ") for row in data.splitlines()]
     equations = [tuple((int(result), list(map(int, equation.split(" "))))) for result, equation in equations]
     return equations
 
+def s(n1, n2):
+    return n1 + n2
+
+def p(n1, n2):
+    return n1 * n2
+
+def c(n1, n2):
+    return int(str(n1) + str(n2))
+
 def check_result_is_correct(current_result, equation_values, final_result, i = 1, allow_concatenation = False):
     if i == len(equation_values):
         return current_result == final_result
     
-    # Try sum
-    result_w_sum = current_result + equation_values[i]
-    if result_w_sum <= final_result:
-        sum_is_correct = check_result_is_correct(result_w_sum, equation_values, final_result, i + 1, allow_concatenation)
-        if sum_is_correct:
-            return True
+    allowed_operator_list = [s, p, c] if allow_concatenation else [s, p]
 
-    # Try product
-    result_w_product = current_result * equation_values[i]
-    if result_w_product <= final_result:
-        product_is_correct = check_result_is_correct(result_w_product, equation_values, final_result, i + 1, allow_concatenation)
-        if product_is_correct:
-            return True
-    
-    # Try concatenation 
-    if allow_concatenation:
-        result_w_concatenation = int(str(current_result) + str(equation_values[i]))
-        if result_w_concatenation <= final_result:
-            concatenation_is_correct = check_result_is_correct(result_w_concatenation, equation_values, final_result, i + 1, allow_concatenation)
-            if concatenation_is_correct:
+    for op in allowed_operator_list:
+        temp_result = op(current_result, equation_values[i])
+        if temp_result <= final_result:
+            op_is_correct = check_result_is_correct(temp_result, equation_values, final_result, i + 1, allow_concatenation)
+            if op_is_correct:
                 return True
-
+            
     return False
 
-def part1(data):
+def run(data, allow_concatenation):
     equations = parse_input(data)
 
     sum_allowed_results = 0
 
     for result, equation_values in equations:
-        if check_result_is_correct(equation_values[0], equation_values, result, 1):
+        if check_result_is_correct(equation_values[0], equation_values, result, 1, allow_concatenation = allow_concatenation):
             sum_allowed_results += result
 
     return sum_allowed_results
+
+def part1(data):
+    return run(data, False)
 
 def part2(data):
-    equations = parse_input(data)
-
-    sum_allowed_results = 0
-
-    for result, equation_values in equations:
-        if check_result_is_correct(equation_values[0], equation_values, result, 1, allow_concatenation = True):
-            sum_allowed_results += result
-
-    return sum_allowed_results
+    return run(data, True)
